27aaa1eb0a7fb0f6155d3dfcffdda0386437b8b7
Bruce Zhang <zttt183525594@gmail.com>
2019-07-16 14:49:03 +0800

    update v1.0

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,4 +6,5 @@
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-# This script will be executed in post-fs-data mode
+# setup selinux policy
+magiskpolicy --live "allow adbd magisk unix_stream_socket { connectto }"
